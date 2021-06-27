bed3791bb7c668b637b73e2dedd51587e53909de
Yeonwoo Lim <1213terry@naver.com>
2021-06-27 17:02:00 +0900

    [CREATE] 프로젝트 생성

---
+++ android/app/src/main/java/com/example/portfolio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.portfolio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() {
+  setPathUrlStrategy();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+      ),
+      body: Center(
+        child: Text('test'),
+      ),
+    );
+  }
+}
