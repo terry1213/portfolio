d8e3a9220038d6285b4f747f44165544272f9153
Yeonwoo Lim <1213terry@naver.com>
2021-06-28 13:54:31 +0900

    [UPDATE]  앱 타이틀 변경

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Terry\'s portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFFF242424),
