c08aeebdd6e65928ae58e891c98dd67906459958
Yeonwoo Lim <1213terry@naver.com>
2021-06-29 19:30:06 +0900

    [UPDATE] 이모티콘 변경

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -32,9 +28,55 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 100,
+            ),
+            Text('Contact me',
+                style: Theme.of(context).textTheme.bodyText1),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(LineIcons.envelopeSquare),
+                  tooltip: 'Email',
+                  splashRadius: 15,
+                  iconSize: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(LineIcons.phoneSquare),
+                  tooltip: 'Phone',
+                  splashRadius: 15,
+                  iconSize: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(LineIcons.linkedin),
+                  tooltip: 'LinkedIn',
+                  splashRadius: 15,
+                  iconSize: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(LineIcons.blogger),
+                  tooltip: 'Blog',
+                  splashRadius: 15,
+                  iconSize: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(LineIcons.githubSquare),
+                  tooltip: 'Github',
+                  splashRadius: 15,
+                  iconSize: 30,
+                ),
+              ],
+            ),
           ],
         ),
-        Image.asset('assets/profile.png', width: size.width * 0.3),
+        Image.asset('assets/profile.png',
+            width: size.width * 0.3 < 300 ? 300 : size.width * 0.3),
       ],
     );
   }
