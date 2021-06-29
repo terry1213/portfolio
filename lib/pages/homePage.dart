c3e3c120fd8fa6f99b35aa4ac3f5536eceee8381
Yeonwoo Lim <1213terry@naver.com>
2021-06-29 19:31:42 +0900

    [FIX] 화면 크기에 따라 overflow 발생하던 문제 해결

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -5,79 +5,84 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
+    return Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            RichText(
-              text: TextSpan(
-                text: '안녕하세요,\n',
-                style: Theme.of(context).textTheme.headline3,
-                children: [
-                  TextSpan(text: 'Flutter 개발자 '),
-                  TextSpan(
-                      text: '임연우',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline2
-                          .apply(color: Colors.lightBlue)),
-                  TextSpan(text: '입니다.'),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Text('Contact me',
-                style: Theme.of(context).textTheme.bodyText1),
-            Row(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(LineIcons.envelopeSquare),
-                  tooltip: 'Email',
-                  splashRadius: 15,
-                  iconSize: 30,
+                RichText(
+                  text: TextSpan(
+                    text: '안녕하세요,\n',
+                    style: Theme.of(context).textTheme.headline3,
+                    children: [
+                      TextSpan(text: 'Flutter 개발자 '),
+                      TextSpan(
+                          text: '임연우',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline2
+                              .apply(color: Colors.lightBlue)),
+                      TextSpan(text: '입니다.'),
+                    ],
+                  ),
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(LineIcons.phoneSquare),
-                  tooltip: 'Phone',
-                  splashRadius: 15,
-                  iconSize: 30,
+                SizedBox(
+                  height: 100,
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(LineIcons.linkedin),
-                  tooltip: 'LinkedIn',
-                  splashRadius: 15,
-                  iconSize: 30,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(LineIcons.blogger),
-                  tooltip: 'Blog',
-                  splashRadius: 15,
-                  iconSize: 30,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(LineIcons.githubSquare),
-                  tooltip: 'Github',
-                  splashRadius: 15,
-                  iconSize: 30,
+                Text('Contact me',
+                    style: Theme.of(context).textTheme.bodyText1),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(LineIcons.envelopeSquare),
+                      tooltip: 'Email',
+                      splashRadius: 15,
+                      iconSize: 30,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(LineIcons.phoneSquare),
+                      tooltip: 'Phone',
+                      splashRadius: 15,
+                      iconSize: 30,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(LineIcons.linkedin),
+                      tooltip: 'LinkedIn',
+                      splashRadius: 15,
+                      iconSize: 30,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(LineIcons.blogger),
+                      tooltip: 'Blog',
+                      splashRadius: 15,
+                      iconSize: 30,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(LineIcons.githubSquare),
+                      tooltip: 'Github',
+                      splashRadius: 15,
+                      iconSize: 30,
+                    ),
+                  ],
                 ),
               ],
             ),
+            Image.asset('assets/profile.png',
+                width: size.width * 0.3 < 300 ? 300 : size.width * 0.3),
           ],
         ),
-        Image.asset('assets/profile.png',
-            width: size.width * 0.3 < 300 ? 300 : size.width * 0.3),
-      ],
+      ),
     );
   }
 }
