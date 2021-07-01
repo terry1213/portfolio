cc17dd5166c8b749cf5d1bceab415a051ea21681
Yeonwoo Lim <1213terry@naver.com>
2021-07-01 16:13:49 +0900

    [FIX] 화면 축소 시 overflow 발생 에러 해결

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -6,7 +6,8 @@ class HomePage extends StatelessWidget {
   Map<String, String> _urls = {
     'Email': 'mailto:1213terry@naver.com',
     'Phone': 'sms:01047888356',
-    'LinkedIn': 'https://www.linkedin.com/in/%EC%97%B0%EC%9A%B0-%EC%9E%84-89291320b/',
+    'LinkedIn':
+        'https://www.linkedin.com/in/%EC%97%B0%EC%9A%B0-%EC%9E%84-89291320b/',
     'Blog': 'https://terry1213.github.io/categories/',
     'Github': 'https://github.com/terry1213',
   };
@@ -16,80 +17,87 @@ class HomePage extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     return Center(
       child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
+        child: Container(
+          height: 750,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RichText(
-                  text: TextSpan(
-                    text: '안녕하세요,\n',
-                    style: Theme.of(context).textTheme.headline3,
-                    children: [
-                      TextSpan(text: 'Flutter 개발자 '),
-                      TextSpan(
-                          text: '임연우',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline2
-                              .copyWith(color: Colors.lightBlue)),
-                      TextSpan(text: '입니다.'),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 100,
-                ),
-                Text('Contact me',
-                    style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold)),
-                Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    IconButton(
-                      onPressed: () => _launchURL('Email'),
-                      icon: Icon(LineIcons.envelopeSquare),
-                      tooltip: 'Email',
-                      splashRadius: 15,
-                      iconSize: 30,
+                    RichText(
+                      text: TextSpan(
+                        text: '안녕하세요,\n',
+                        style: Theme.of(context).textTheme.headline2,
+                        children: [
+                          TextSpan(text: 'Flutter 개발자 '),
+                          TextSpan(
+                              text: '임연우',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline1
+                                  .copyWith(color: Colors.lightBlue)),
+                          TextSpan(text: '입니다.'),
+                        ],
+                      ),
                     ),
-                    IconButton(
-                      onPressed: () => _launchURL('Phone'),
-                      icon: Icon(LineIcons.phoneSquare),
-                      tooltip: 'Phone',
-                      splashRadius: 15,
-                      iconSize: 30,
+                    SizedBox(
+                      height: 100,
                     ),
-                    IconButton(
-                      onPressed: () => _launchURL('LinkedIn'),
-                      icon: Icon(LineIcons.linkedin),
-                      tooltip: 'LinkedIn',
-                      splashRadius: 15,
-                      iconSize: 30,
-                    ),
-                    IconButton(
-                      onPressed: () => _launchURL('Blog'),
-                      icon: Icon(LineIcons.blogger),
-                      tooltip: 'Blog',
-                      splashRadius: 15,
-                      iconSize: 30,
-                    ),
-                    IconButton(
-                      onPressed: () => _launchURL('Github'),
-                      icon: Icon(LineIcons.githubSquare),
-                      tooltip: 'Github',
-                      splashRadius: 15,
-                      iconSize: 30,
+                    Text('Contact me',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () => _launchURL('Email'),
+                          icon: Icon(LineIcons.envelopeSquare),
+                          tooltip: 'Email',
+                          splashRadius: 15,
+                          iconSize: 30,
+                        ),
+                        IconButton(
+                          onPressed: () => _launchURL('Phone'),
+                          icon: Icon(LineIcons.phoneSquare),
+                          tooltip: 'Phone',
+                          splashRadius: 15,
+                          iconSize: 30,
+                        ),
+                        IconButton(
+                          onPressed: () => _launchURL('LinkedIn'),
+                          icon: Icon(LineIcons.linkedin),
+                          tooltip: 'LinkedIn',
+                          splashRadius: 15,
+                          iconSize: 30,
+                        ),
+                        IconButton(
+                          onPressed: () => _launchURL('Blog'),
+                          icon: Icon(LineIcons.blogger),
+                          tooltip: 'Blog',
+                          splashRadius: 15,
+                          iconSize: 30,
+                        ),
+                        IconButton(
+                          onPressed: () => _launchURL('Github'),
+                          icon: Icon(LineIcons.githubSquare),
+                          tooltip: 'Github',
+                          splashRadius: 15,
+                          iconSize: 30,
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                Image.asset('assets/profile.png', width: 600),
               ],
             ),
-            Image.asset('assets/profile.png',
-                width: size.width * 0.3 < 300 ? 300 : size.width * 0.3),
-          ],
+          ),
         ),
       ),
     );
