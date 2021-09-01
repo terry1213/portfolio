5e41475f560aae952818db940add33f167675caa
Yeonwoo Lim <1213terry@naver.com>
2021-09-01 21:59:45 +0900

    [UPDATE] 사이드 바 삭제, 상단 바로 변경

--- lib/pages/templatePage.dart
+++ lib/pages/templatePage.dart
@@ -1,8 +1,5 @@
-import 'dart:math' as math;
-import 'package:collapsible_sidebar/collapsible_sidebar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:portfolio/pages/aboutPage.dart';
 import 'package:portfolio/pages/blogPage.dart';
 import 'package:portfolio/pages/careerPage.dart';
@@ -18,8 +15,6 @@ class TemplatePage extends StatefulWidget {
 
 class _TemplatePageState extends State<TemplatePage> {
   final PageController _pageController = PageController();
-  final AssetImage _avatarImg = AssetImage('assets/profile_sidebar.jpg');
-  int _currentPageIndex = 0;
   final List<Widget> pages = const <Widget>[
     HomePage(),
     AboutPage(),
@@ -32,82 +27,70 @@ class _TemplatePageState extends State<TemplatePage> {
 
   @override
   Widget build(BuildContext context) {
-    List<CollapsibleItem> _items = [
-      CollapsibleItem(
-        text: 'Home',
-        icon: Icons.home,
-        onPressed: () => _pageController.animateToPage(0,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 0 ? true : false,
-      ),
-      CollapsibleItem(
-        text: 'About',
-        icon: Icons.person,
-        onPressed: () => _pageController.animateToPage(1,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 1 ? true : false,
-      ),
-      CollapsibleItem(
-        text: 'Skill',
-        icon: Icons.bar_chart_rounded,
-        onPressed: () => _pageController.animateToPage(2,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 2 ? true : false,
-      ),
-      CollapsibleItem(
-        text: 'Career',
-        icon: Icons.work,
-        onPressed: () => _pageController.animateToPage(3,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 3 ? true : false,
-      ),
-      CollapsibleItem(
-        text: 'Project',
-        icon: LineIcons.projectDiagram,
-        onPressed: () => _pageController.animateToPage(4,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 4 ? true : false,
-      ),
-      CollapsibleItem(
-        text: 'Blog',
-        icon: LineIcons.bloggerB,
-        onPressed: () => _pageController.animateToPage(5,
-            duration: Duration(milliseconds: 700), curve: Curves.easeInOut),
-        isSelected: _currentPageIndex == 5 ? true : false,
-      ),
-    ];
     return Scaffold(
-      body: CollapsibleSidebar(
-        items: _items,
-        avatarImg: _avatarImg,
-        title: '임연우',
-        body: PageView(
-          controller: _pageController,
-          scrollDirection: Axis.vertical,
-          physics: NeverScrollableScrollPhysics(),
-          children: pages,
-          onPageChanged: (index) {
-            setState(() {
-              _currentPageIndex = index;
-            });
-          },
+      appBar: PreferredSize(
+        child: Center(
+          child: Container(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(),
+                ),
+                TextButton(
+                  child: Text('Home'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(0,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+                TextButton(
+                  child: Text('About'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(1,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+                TextButton(
+                  child: Text('Skill'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(2,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+                TextButton(
+                  child: Text('Career'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(3,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+                TextButton(
+                  child: Text('Project'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(4,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+                TextButton(
+                  child: Text('Blog'),
+                  style: ButtonStyle(fixedSize: MaterialStateProperty.all(Size.fromWidth(80)),),
+                  onPressed: () => _pageController.animateToPage(5,
+                      duration: Duration(milliseconds: 700),
+                      curve: Curves.easeInOut),
+                ),
+              ],
+            ),
+            color: Color(0xFFF242424),
+            width: 1360,
+          ),
         ),
-        toggleTitle: 'Close',
-        backgroundColor: Colors.black,
-        selectedTextColor: Colors.white,
-        selectedIconColor: Colors.white,
-        selectedIconBox: Colors.transparent,
-        unselectedTextColor: Colors.grey,
-        unselectedIconColor: Colors.grey,
-        iconSize: 30,
-        minWidth: 70,
-        maxWidth: 200,
-        screenPadding: 0,
-        borderRadius: 0,
-        textStyle: textTheme.headline6,
-        titleStyle: textTheme.headline6!
-            .copyWith(fontWeight: FontWeight.bold, fontSize: 25),
-        toggleTitleStyle: textTheme.headline6,
+        preferredSize: Size.fromHeight(90),
+      ),
+      body: PageView(
+        controller: _pageController,
+        scrollDirection: Axis.horizontal,
+        physics: NeverScrollableScrollPhysics(),
+        children: pages,
       ),
     );
   }
