58dc1b3a3ee8e65b6537d109081470034a0ae57f
Yeonwoo Lim <1213terry@naver.com>
2021-10-03 21:31:46 +0900

    [UPDATE] theme 데이터 분리 및 easy_dynamic_theme 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/pages/templatePage.dart';
+import 'package:portfolio/utils/theme_data.dart';
 import 'package:universal_html/html.dart' as html;
 import 'package:url_strategy/url_strategy.dart';
 
@@ -10,7 +11,7 @@ import 'controller/carouselIndexController.dart';
 void main() {
   setPathUrlStrategy();
   Get.put(CarouselIndexController(index: 0), tag: 'blog');
-  runApp(MyApp());
+  runApp(EasyDynamicThemeWidget(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,38 +20,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: '임연우 Flutter Developer',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Color(0xFFF242424),
-        ),
-        bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.white24,
-        ),
-        bottomAppBarColor: Colors.white24,
-        dividerColor: Colors.white24,
-        iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
-        scaffoldBackgroundColor: Color(0xFFF242424),
-        textTheme: TextTheme(
-          headline1: GoogleFonts.nanumPenScript(fontSize: 96),
-          headline2: GoogleFonts.nanumPenScript(fontSize: 60),
-          headline3: GoogleFonts.nanumPenScript(fontSize: 48),
-          headline4: GoogleFonts.nanumPenScript(fontSize: 34),
-          headline5: GoogleFonts.notoSans(fontSize: 24),
-          headline6: GoogleFonts.notoSans(fontSize: 20),
-          bodyText1: GoogleFonts.notoSans(fontSize: 20),
-          bodyText2: GoogleFonts.notoSans(fontSize: 15, height: 2),
-        ).apply(displayColor: Colors.white, bodyColor: Colors.white),
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            overlayColor: MaterialStateProperty.all(Colors.transparent),
-            textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            visualDensity: VisualDensity.compact,
-          ),
-        ),
-      ),
+      darkTheme: darkThemeData,
+      theme: lightThemeData,
+      themeMode: EasyDynamicTheme.of(context).themeMode!,
       home: InteractiveViewer(
         child: TemplatePage(),
         scaleEnabled: getPlatformType() == PlatformType.desktop ? false : true,
---
+++ lib/utils/theme_data.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var lightThemeData = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Color(0xFFFDBDBDB),
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+  ),
+  bottomAppBarTheme: BottomAppBarTheme(
+    color: Colors.black26,
+  ),
+  bottomAppBarColor: Colors.black26,
+  brightness: Brightness.light,
+  dividerColor: Colors.black26,
+  highlightColor: Colors.transparent,
+  hoverColor: Colors.transparent,
+  iconTheme: IconThemeData(
+    color: Colors.black,
+  ),
+  scaffoldBackgroundColor: Color(0xFFFDBDBDB),
+  splashColor: Colors.transparent,
+  textTheme: textThemeLight,
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(Colors.black),
+      overlayColor: MaterialStateProperty.all(Colors.transparent),
+      textStyle: MaterialStateProperty.all(
+          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      visualDensity: VisualDensity.compact,
+    ),
+  ),
+);
+
+var darkThemeData = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Color(0xFFF242424),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+  ),
+  bottomAppBarTheme: BottomAppBarTheme(
+    color: Colors.white24,
+  ),
+  bottomAppBarColor: Colors.white24,
+  brightness: Brightness.dark,
+  dividerColor: Colors.white24,
+  highlightColor: Colors.transparent,
+  hoverColor: Colors.transparent,
+  iconTheme: IconThemeData(
+    color: Colors.white,
+  ),
+  scaffoldBackgroundColor: Color(0xFFF242424),
+  splashColor: Colors.transparent,
+  textTheme: textThemeDark,
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      overlayColor: MaterialStateProperty.all(Colors.transparent),
+      textStyle: MaterialStateProperty.all(
+          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      visualDensity: VisualDensity.compact,
+    ),
+  ),
+);
+
+var textThemeLight = TextTheme(
+  headline1: GoogleFonts.nanumPenScript(fontSize: 96, color: Colors.black),
+  headline2: GoogleFonts.nanumPenScript(fontSize: 60, color: Colors.black),
+  headline3: GoogleFonts.nanumPenScript(fontSize: 48, color: Colors.black),
+  headline4: GoogleFonts.nanumPenScript(fontSize: 34, color: Colors.black),
+  headline5: GoogleFonts.notoSans(fontSize: 24, color: Colors.black),
+  headline6: GoogleFonts.notoSans(fontSize: 20, color: Colors.black),
+  bodyText1: GoogleFonts.notoSans(fontSize: 20, color: Colors.black),
+  bodyText2: GoogleFonts.notoSans(fontSize: 15, height: 2, color: Colors.black),
+);
+
+var textThemeDark = TextTheme(
+  headline1: GoogleFonts.nanumPenScript(fontSize: 96, color: Colors.white),
+  headline2: GoogleFonts.nanumPenScript(fontSize: 60, color: Colors.white),
+  headline3: GoogleFonts.nanumPenScript(fontSize: 48, color: Colors.white),
+  headline4: GoogleFonts.nanumPenScript(fontSize: 34, color: Colors.white),
+  headline5: GoogleFonts.notoSans(fontSize: 24, color: Colors.white),
+  headline6: GoogleFonts.notoSans(fontSize: 20, color: Colors.white),
+  bodyText1: GoogleFonts.notoSans(fontSize: 20, color: Colors.white),
+  bodyText2: GoogleFonts.notoSans(fontSize: 15, height: 2, color: Colors.white),
+);
