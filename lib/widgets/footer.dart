6922c7c77cef715bd9d69d7cd22edcc5b140e4bf
Yeonwoo Lim <1213terry@naver.com>
2021-10-04 21:04:51 +0900

    [CREATE] Footer 생성

---
+++ lib/widgets/footer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Container(
+      width: screenSize.width > 1360 ? screenSize.width : 1360,
+      padding: EdgeInsets.all(20),
+      color: Theme.of(context).bottomAppBarColor,
+      child: Column(
+        children: [
+          Divider(),
+          SizedBox(height: 10),
+          Text(
+            'Copyright © 2021 | Yeonwoo Lim',
+            style: TextStyle(
+              color: Colors.blueGrey[300],
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
