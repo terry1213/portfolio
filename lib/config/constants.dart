f6cfaca8dfdd1175333cdc1ff9baaecf71f7e105
Yeonwoo Lim <1213terry@naver.com>
2023-03-02 19:39:25 +0900

    Chore: base url 변경

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,2 +1,2 @@
 const baseUrl =
-    'https://gist.githubusercontent.com/terry1213/43658decb89487721c7f9437705f468d/raw/290d3ff5f4b2f4e890c1f695d5470446bed09529/';
+    'https://gist.githubusercontent.com/terry1213/43658decb89487721c7f9437705f468d/raw/d7cea87076f7c256d37f0e065964d2fa3e043552/';
